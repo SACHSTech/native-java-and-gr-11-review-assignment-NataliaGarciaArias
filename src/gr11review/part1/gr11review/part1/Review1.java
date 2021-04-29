6ca9e0fd39f9651017f799e6154be182c0772e10
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-04-29 19:58:46 -0400

    started code and declared variables

--- src/gr11review/part1/gr11review/part1/Review1.java
+++ src/gr11review/part1/gr11review/part1/Review1.java
@@ -1,1 +1,17 @@
 package gr11review.part1;
+import java.io.*;
+
+public class Review1{
+    public static void main(String[] args) throws IOException{
+
+        //Declare variables
+
+        int numMonth;
+        int numDay;
+        int numDayOfYear;
+
+        BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}

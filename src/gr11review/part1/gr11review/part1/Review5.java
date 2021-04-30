9980264638c36b4f11df4fd3caa98531b7b4b346
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-04-30 10:57:52 -0400

    created variables + user input prompts

--- src/gr11review/part1/gr11review/part1/Review5.java
+++ src/gr11review/part1/gr11review/part1/Review5.java
@@ -6,11 +6,23 @@ import java.io.*;
  */
 public class Review5{
     public static void main(String[] args) throws IOException{
+        BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
         //Create variables
+        double investedAmount;
+        double interestRate;
+        double targetAmount;
 
 
         //Prompt for user input
+        System.out.print("Enter the yearly invested amount: ");
+        investedAmount = Double.parseDouble(key.readLine());
+
+        System.out.print("Enter the compound interest rate: ");
+        interestRate = Double.parseDouble(key.readLine());
+
+        System.out.print("Enter the target amount: ");
+        targetAmount = Double.parseDouble(key.readLine());
 
 
 

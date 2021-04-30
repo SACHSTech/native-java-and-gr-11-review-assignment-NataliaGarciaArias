b70c741f526f7c0f0aaffcd83a62f2dfee7f3acf
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-04-29 21:47:31 -0400

    variables and user input

--- src/gr11review/part1/gr11review/part1/Review1.java
+++ src/gr11review/part1/gr11review/part1/Review1.java
@@ -4,14 +4,20 @@ import java.io.*;
 public class Review1{
     public static void main(String[] args) throws IOException{
 
-        //Declare variables
+        //Declare Variables
 
         int numMonth;
         int numDay;
-        int numDayOfYear;
+        int[] listMonthsInDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int numDayOfYear = 0;
 
         BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
+        System.out.print("Enter the month number: ");
+        numMonth = Integer.parseInt(key.readLine());
+
+        System.out.print("Enter the day number: ");
+        numDay = Integer.parseInt(key.readLine());
 
     }
 }

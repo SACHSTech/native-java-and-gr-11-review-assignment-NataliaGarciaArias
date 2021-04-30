a2028dbcd186deec6c97d753e338d8bd2411cb29
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-04-30 00:36:33 -0400

    second loop and comments

--- src/gr11review/part1/gr11review/part1/Review3.java
+++ src/gr11review/part1/gr11review/part1/Review3.java
@@ -7,11 +7,20 @@ import java.io.*;
 
  public class Review3{
      public static void main(String[] args) throws IOException{
+        
+        //Print odd numbers from 20 - 100
         for (int i = 20; i <= 100; i++){
             if (i % 2 != 0) {
                 System.out.print(i + " ");
             }
-
          }
+
+        //Blank line to separate
+        System.out.println(" \n");
+
+        //Print numbers from 29 - 2 in decreasing order
+        for (int d = 29; d >= 2; d--){
+            System.out.print(d + " ");
+        }
      }
  }

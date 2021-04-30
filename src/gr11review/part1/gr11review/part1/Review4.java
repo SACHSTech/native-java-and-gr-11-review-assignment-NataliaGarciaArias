28f902f1f596fedec2107389896c42c123291411
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-04-30 00:56:22 -0400

    added variables + input for loop

--- src/gr11review/part1/gr11review/part1/Review4.java
+++ src/gr11review/part1/gr11review/part1/Review4.java
@@ -1,11 +1,31 @@
 package gr11review.part1;
 import java.io.*;
+import java.text.DecimalFormat.*;
 /**
  * Block comment here
  * @author N. Garcia-Arias
  */
 public class Review4{
     public static void main(String[] args) throws IOException{
-        
+
+        //Create variables
+        int numItems;
+        double sumPrice = 0;
+
+        BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+        //Ask user for # of items they want to purchase
+        System.out.print("How many times do you want to buy? ");
+        numItems = Integer.parseInt(key.readLine());
+
+        //Get prices
+        for(int i = 1 ;i <= numItems;i++){
+            System.out.print("Enter the price for item "+ i+ ": ");
+            sumPrice += Double.parseDouble(key.readLine());
+        }
+
+
+
+        //Print total, tax, and grand total
     }
 }

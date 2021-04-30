83bc91f194f8180b64c94516ea8190e36ce2aa5e
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-04-30 13:30:52 -0400

    added comments

--- src/gr11review/part1/gr11review/part1/Review5.java
+++ src/gr11review/part1/gr11review/part1/Review5.java
@@ -1,23 +1,25 @@
 package gr11review.part1;
 import java.io.*;
 /**
- * Block comment
+ * This program calculates the years required to
+ * reach a target investment amount using inputted information.
  * @author N.Garcia-Arias
  */
 public class Review5{
     public static void main(String[] args) throws IOException{
         BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
-        //Create variables
+        //Creates variables for user input
         double investedAmount = 0;
         double interestRate = 0;
         double targetAmount = 0;
+
+        //Creates variables for calculations
         double currentAmount = 0;
         double compoundedAmount = 0;
         int targetTime = 0;
 
-
-        //Prompt for user input
+        //Asks user for the yearly invested amount, interest rate, and target amount
         System.out.print("Enter the yearly invested amount: ");
         investedAmount = Double.parseDouble(key.readLine());
 
@@ -27,11 +29,12 @@ public class Review5{
         System.out.print("Enter the target amount: ");
         targetAmount = Double.parseDouble(key.readLine());
 
-        //Calculate
+        //Calculates for the compounded amount and years required to reach target amount
         while (currentAmount < targetAmount){
 
             compoundedAmount = (interestRate/100)*(currentAmount + investedAmount);
-            currentAmount += compoundedAmount + investedAmount;
+            currentAmount += (compoundedAmount + investedAmount);
+
             targetTime++;
         }
 

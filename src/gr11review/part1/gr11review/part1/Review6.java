a93baf0247d0918447353acd825b442ea6b1d3a8
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-04-30 13:42:44 -0400

    created variables and started while looop

--- src/gr11review/part1/gr11review/part1/Review6.java
+++ src/gr11review/part1/gr11review/part1/Review6.java
@@ -7,6 +7,18 @@ import java.io.*;
  */
  public class Review6{
      public static void main(String[] args) throws IOException{
+         BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+         //Creates variables
+         double itemPrice = 1;
+         double sumPrice = 0;
+         int itemCount = 1;
+
+         while (itemPrice != 0){
+             System.out.print("Enter the price item " +itemCount+ ": ");
+             sumPrice += Double.parseDouble(key.readLine());
+
+         }
 
      }
  }

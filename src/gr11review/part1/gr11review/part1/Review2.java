f6be0955261b75908d037a64c9cf00130ebb6a95
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-04-29 23:34:35 -0400

    print options and ask for input

--- src/gr11review/part1/gr11review/part1/Review2.java
+++ src/gr11review/part1/gr11review/part1/Review2.java
@@ -8,7 +8,14 @@ import java.io.*;
  public class Review2{
      public static void main(String[] args) throws IOException{
 
-       
+        //Print Options
+        System.out.println("0 - print a joke about your hair");
+        System.out.println("1 - print a joke about your feet");
+        System.out.println("2 - print a joke about your clothes");
+        System.out.println("3 - print a joke about your teacher");
+
+        //Ask for user input
+        System.out.println("Choose a menu option: ");
 
 
      }

fc6ed168c105c89ef789e676e3d302dfb5729aa8
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-04-29 22:20:42 -0400

    block comment and spacing

--- src/gr11review/part1/gr11review/part1/Review1.java
+++ src/gr11review/part1/gr11review/part1/Review1.java
@@ -1,17 +1,21 @@
 package gr11review.part1;
 import java.io.*;
 
+/**
+ * This program Review1.java lets the user enter a month number and day,
+ * then prints the day of the year in which it lies on. 
+ * @author: N. Garcia-Arias
+ * 
+ */
 public class Review1{
     public static void main(String[] args) throws IOException{
 
-        //Declare variables and create an array storing the number of days in a month
-
+        //Create variables and create an array storing the number of days in a month
         int numMonth;
         int numDay;
         int[] listDaysInMonths = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int sumDaysInMonths = 0;
         int numDayOfYear;
-
         BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
         
         //Ask user to for the month and day numbers
@@ -22,9 +26,7 @@ public class Review1{
         numDay = Integer.parseInt(key.readLine());
 
         for (int i = 0; i < (numMonth - 1); i++){
-
             sumDaysInMonths += listDaysInMonths[i];
-        
         }
         
         //Print the day of the year

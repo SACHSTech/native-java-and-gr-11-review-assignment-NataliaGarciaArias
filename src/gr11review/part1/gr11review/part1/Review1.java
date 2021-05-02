4f6f8c7344f37338b43175930d32b3c8dfbfa1c9
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-05-02 18:33:50 -0400

    finalized review 1

--- src/gr11review/part1/gr11review/part1/Review1.java
+++ src/gr11review/part1/gr11review/part1/Review1.java
@@ -3,12 +3,14 @@ import java.io.*;
 
 /**
  * This program Review1.java lets the user enter a month number and day,
- * then prints the day of the year in which it lies on. Assuming that it is not a leap year.
+ * then prints the day of the year in which it lies on. 
+ * Assuming that it is not a leap year.
  * @author: N. Garcia-Arias
  * 
  */
 public class Review1{
     public static void main(String[] args) throws IOException{
+        BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
         //Create variables and create an array storing the number of days in a month
         int numMonth;
@@ -16,7 +18,7 @@ public class Review1{
         int[] listDaysInMonths = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int sumDaysInMonths = 0;
         int numDayOfYear;
-        BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+        
         
         //Ask user to for the month and day numbers
         System.out.print("Enter the month number: ");
@@ -30,7 +32,7 @@ public class Review1{
             sumDaysInMonths += listDaysInMonths[i];
         }
         
-        //Add and print results
+        //Add and output results
         numDayOfYear = numDay + sumDaysInMonths;
         System.out.println(numDayOfYear);
     }

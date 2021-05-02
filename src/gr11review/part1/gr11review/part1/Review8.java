d1d4c9ba0fec6d3bb7a02e419aff83cb41b2cdc5
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-05-02 17:34:01 -0400

    added comments

--- src/gr11review/part1/gr11review/part1/Review8.java
+++ src/gr11review/part1/gr11review/part1/Review8.java
@@ -16,16 +16,21 @@ public class Review8{
 
         for (int i = 0; i < 1000; i++){
 
+            //Generate three random numbers between 0 and 8
             random1 = (int)(Math.random() * ((8 - 0) + 1));
             random2 = (int)(Math.random() * ((8 - 0) + 1));
             random3 = (int)(Math.random() * ((8 - 0) + 1));
 
+            //Count times the slot machine pulls 3 identical numbers
             if (random1 == random2 && random2 == random3 && random1 == random3){
                 tripleCount++;
             }
 
+            //Print the three results on a single line
             System.out.println(random1 + " " + random2 + " " + random3);
         }
+
+        //Output the total triple count
         System.out.println(tripleCount);
     }
 }

c19fca73d9bb627d6a3e03f0dab75b3906992543
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-05-02 16:50:02 -0400

    variables + slot machine

--- src/gr11review/part1/gr11review/part1/Review8.java
+++ src/gr11review/part1/gr11review/part1/Review8.java
@@ -7,6 +7,20 @@ import java.io.*;
  */
 public class Review8{
     public static void main(String[] args) throws IOException{
-        
+
+        //Create variables
+        int random1 = 0;
+        int random2 = 0;
+        int random3 = 0;
+
+        for(int i = 0; i < 1000; i++){
+
+            random1 = (int)(Math.random() * ((8 - 0) + 1));
+            random2 = (int)(Math.random() * ((8 - 0) + 1));
+            random3 = (int)(Math.random() * ((8 - 0) + 1));
+
+            System.out.println(random1 + " " + random2 + " " + random3);
+        }
+
     }
 }

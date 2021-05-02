009e9ad0021c690bb8ca68968b7f69446fc458cf
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-05-02 15:37:12 -0400

    started code

--- src/gr11review/part1/gr11review/part1/Review7.java
+++ src/gr11review/part1/gr11review/part1/Review7.java
@@ -1,1 +1,18 @@
 package gr11review.part1;
+import java.io.*;
+/**
+ * Block comment
+ * @author N. Garcia-Arias
+ * 
+ */
+
+public class Review7{
+    public static void main(String[] args) throws IOException{
+        BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+        
+        String theSentence;
+        int sentenceLength;
+        
+        theSentence = key.readLine();
+    }
+}

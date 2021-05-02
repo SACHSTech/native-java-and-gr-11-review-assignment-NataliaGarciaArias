bfff0577e6d76b4da736c5e406bae1cb59d5a9d6
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-05-02 19:26:46 -0400

    finalized review 3

--- src/gr11review/part1/gr11review/part1/Review3.java
+++ src/gr11review/part1/gr11review/part1/Review3.java
@@ -1,22 +1,24 @@
 package gr11review.part1;
 import java.io.*;
 /**
- * Block comment here
+ * This program Review3.java prints odd numbers from 20 to 100
+ * and numbers from 29 to 2 in decreasing order.
  * @author: N. Garcia-Arias
  */
-
  public class Review3{
      public static void main(String[] args) throws IOException{
         
-        //Print odd numbers from 20 - 100
+        //Print odd numbers from 20 to 100
         for (int i = 20; i <= 100; i++){
-            if (i % 2 != 0) {
+            if (i % 2 != 0){
                 System.out.println(i);
             }
          }
+
         //Print blank line that separates both loops
         System.out.println("");
-        //Print numbers from 29 - 2 in decreasing order
+
+        //Print numbers from 29 to 2 in decreasing order
         for (int d = 29; d >= 2; d--){
             System.out.println(d);
         }

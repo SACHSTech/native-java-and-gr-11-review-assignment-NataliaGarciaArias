048dd903e81816f72476cea6d7dd6d40ea95d537
NataliaGarciaArias <natalia.garciaarias21@ycdsbk12.ca>
2021-05-02 17:30:49 -0400

    added the triple count

--- src/gr11review/part1/gr11review/part1/Review8.java
+++ src/gr11review/part1/gr11review/part1/Review8.java
@@ -12,15 +12,20 @@ public class Review8{
         int random1 = 0;
         int random2 = 0;
         int random3 = 0;
+        int tripleCount = 0;
 
-        for(int i = 0; i < 1000; i++){
+        for (int i = 0; i < 1000; i++){
 
             random1 = (int)(Math.random() * ((8 - 0) + 1));
             random2 = (int)(Math.random() * ((8 - 0) + 1));
             random3 = (int)(Math.random() * ((8 - 0) + 1));
 
+            if (random1 == random2 && random2 == random3 && random1 == random3){
+                tripleCount++;
+            }
+
             System.out.println(random1 + " " + random2 + " " + random3);
         }
-
+        System.out.println(tripleCount);
     }
 }
